12bd19e5a1dad42f19dff36722eebac5ad351ba2
chetanananthu <chetanananthu@gmail.com>
2024-04-26 12:11:44 +0530

    updated java file

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        StringBuffer sb=new StringBuffer("racecar");
+        System.out.println(sb.reverse());
     }
 }
